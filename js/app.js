e830022105063b1e5bb2256d1a56851fa3e06064
vpolulich <vitalijfrontdev@gmail.com>
2016-06-20 01:07:29 +0300

    test

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 /*model*/
-function GameModel(size, similar) {
-    this.size = size;
-    this.similar = similar;
+function GameModel(configs) {
+    this.configs = $.extend({
+        container: $('#zone'),
+        size: 4,
+        similar: 2,
+        timeStartShow: 2000,
+        timeLimit: 2
+    }, configs);
     this.compareAr = [];
     this.arrColors = [];
     this.arrProp = [];
     this.successArr = [];
-}
+};
 GameModel.prototype = {
     _createArrColors: function () {
         var a1 = [],
-                a2 = (this.size * this.size) / this.similar;
+                a2 = (this.configs.size * this.configs.size) / this.configs.similar;
         for (var j = 0; j < a2; j++) {
             a1.push("#" + Math.random().toString(16).slice(2, 8));
         }
-        for (var i = 0; i < this.similar; i++) {
+        for (var i = 0; i < this.configs.similar; i++) {
             this.arrColors = a1.concat(this.arrColors);
         }
         this.arrColors.sort(function () {
@@ -22,24 +27,24 @@ GameModel.prototype = {
         });
     },
     _buildArr: function () {
-        for (var i = 0; i < this.size * this.size; i++) {
+        for (var i = 0; i < this.configs.size * this.configs.size; i++) {
             $("<div/>", {
                 'style': "background-color: " + this.arrColors[i] + ";",
                 "class": "square"
-            }).appendTo("#zone");
+            }).appendTo(this.configs.container);
         }
         this.items = $('.square');
     },
     _setCubeWidth: function () {
         var itemW = this.items.outerWidth(true),
-                zoneW = itemW * this.size;
-        $("#zone").css({
+                zoneW = itemW * this.configs.size;
+        $(this.configs.container).css({
             width: zoneW
         });
     },
     _compareItems: function () {
         if (this._checkOnSimilar()) {
-            if (this.arrProp.length === this.similar) {
+            if (this.arrProp.length === this.configs.similar) {
                 for (var i in this.arrProp) {
                     this.successArr.push(1);
                 }
@@ -47,7 +52,7 @@ GameModel.prototype = {
             }
         } else {
             for (var i in this.arrProp) {
-                this._refresh(this.items.eq(this.arrProp[i]));
+                this._refreshDifferent(this.items.eq(this.arrProp[i]));
             }
             this.arrProp = [];
         }
@@ -69,34 +74,129 @@ GameModel.prototype = {
         }
         return true;
     },
-    _refresh: function (diffItem) {
+    _refreshDifferent: function (diffItem) {
         setTimeout(function () {
             $(diffItem).css('background-color', '').removeClass('is-visible');
         }, 450);
     },
-    _refreshAll: function () {
+    _refreshAfterStart: function () {
         var items = this.items;
         setTimeout(function () {
             items.css({
                 'background-color': ''
             });
-        }, 1000);
+        }, this.configs.timeStartShow);
+    },
+    _refreshGame: function () {
+        this.clickOnElement;
+        this.arrProp = [];
+        this.successArr = [];
+        this.arrColors = [];
+        this.items.remove();
+        this._createArrColors();
+        this._buildArr();
+        this._refreshAfterStart();
+        clearInterval(globalInterval);
+        $('#timerIndicator').css({
+            'width': '100%',
+            'background-color': 'green'
+        });
+        this._setTimeLimit();
+    },
+    _buttonStatus: function () {
+        $('.startGame').attr('disabled', 'disabled');
+        $('.refreshGame').removeAttr('disabled');
+    },
+    _setTimeLimit: function () {
+        var a = this.configs.timeLimit,
+                b = a * 60 * 100,
+                c = 100 / b,
+                d = 0,
+                e = 0,
+                f = $('#timerIndicator'),
+                g = $('#timer'),
+                self = this;
+        setTimeout(function () {
+            globalInterval = setInterval(function () {
+                e += c;
+                d = 100 - e;
+                f.css({
+                    'width': d + '%'
+                });
+                if (d > 0) {
+                    f.css({
+                        'background-color': '#ca4100'
+                    });
+                }
+                if (d > 25) {
+                    f.css({
+                        'background-color': '#e46c33'
+                    });
+                }
+                if (d > 50) {
+                    f.css({
+                        'background-color': '#b9981e'
+                    });
+                }
+                if (d > 75) {
+                    f.css({
+                        'background-color': 'green'
+                    });
+                }
+                if (d < 0) {
+                    self._pastLimitedTime();
+                }
+            }, 10);
+        }, this.configs.timeStartShow);
+        g.css('visibility', 'visible');
+
+    },
+    _pastLimitedTime: function () {
+        if (confirm('Press ok to play again')) {
+            $('#timerIndicator').css({
+                'width': '100%',
+                'background-color': 'green'
+            });
+            this._refreshGame();
+            clearInterval(globalInterval);
+        } else {
+            clearInterval(globalInterval);
+            $(document).off('click', '.square');
+            $('.refreshGame').attr('disabled', 'disabled');
+        }
     },
     _endGame: function () {
-        if (this.successArr.length === (this.size * this.size)) {
-            alert('Finish');
+        if (this.successArr.length === (this.configs.size * this.configs.size)) {
+            if (confirm('Finish, play again?')) {
+                this._refreshGame();
+            } else {
+                clearInterval(globalInterval);
+                $(document).off('click', '.square');
+                $('.refreshGame').attr('disabled', 'disabled');
+            }
         }
     }
 };
 
 
 function GameController() {
-    _model = new GameModel(3, 3);
+    _model = new GameModel({
+        container: $('#zone'),
+        size: 3,
+        similar: 3,
+        timeStartShow: 2000,
+        timeLimit: 1 //minutes
+    });
     this.startGame = function () {
+        _model._buttonStatus();
         _model._createArrColors();
         _model._buildArr();
         _model._setCubeWidth();
-        _model._refreshAll();
+        _model._refreshAfterStart();
+        _model._setTimeLimit();
+    };
+    this.refreshGame = function () {
+        _model._refreshGame();
     };
     this.clickOnElement = function () {
         $(document).on('click', '.square', function () {
@@ -109,12 +209,16 @@ function GameController() {
 }
 GameController.prototype.init = function () {
     var self = this;
-    $(document).on('click', '#startGame', function () {
+    $('.startGame').on('click', function () {
         self.startGame();
+    });
+    $('.refreshGame').on('click', function () {
+        self.refreshGame();
     });
     self.clickOnElement();
 };
 
+var globalInterval;
 $(document).ready(function () {
     new GameController().init();
 });
